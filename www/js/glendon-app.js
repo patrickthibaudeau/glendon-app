152ae5ef1ee4a18732e8a0680dc3a4152e976a54
unknown <thibaud@GL-ITS-2GMHV12.yorku.yorku.ca>
2015-04-17 13:51:10 -0400

    Added all Phonegap files

---
+++ www/js/glendon-app.js
@@ -0,0 +1,9 @@
+function init_glendon_app() {
+    $('#eclassurl').removeClass('ui-btn');
+    $('#libraryurl').removeClass('ui-btn');
+    $('#findapersonurl').removeClass('ui-btn');
+    $('#studentservicesurl').removeClass('ui-btn');
+    
+    //load header
+    $('.app-header').load('header.html');
+}
