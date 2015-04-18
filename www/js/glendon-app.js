8a3817c51efcfeba0537e5605808acd89faaa732
Patrick Thibaudeau <patrickt@oohoo.biz>
2015-04-17 23:17:10 -0400

    Added language toggle

--- www/js/glendon-app.js
+++ www/js/glendon-app.js
@@ -1,10 +1,38 @@
 function init_glendon_app() {
-    $('#eclassurl').removeClass('ui-btn');
-    $('#libraryurl').removeClass('ui-btn');
-    $('#findapersonurl').removeClass('ui-btn');
-    $('#studentservicesurl').removeClass('ui-btn');
-    
+
     //load header
     $('.app-header').load('header.html');
     $('.app-footer').load('footer.html');
+
+    $('a').removeClass();
+
+    $('#eclassurl').click(function () {
+        var ref = window.open('http://eclass.glendon.yorku.ca', '_system', 'location=yes');
+    });
+    var lang = $('#lang').val();
+    lang_toggle(lang);
+}
+
+function init_glendon_header() {
+    $('.en_btn').click(function () {
+        $('#lang').val('en');
+        lang_toggle('en');
+    });
+    $('.fr_btn').click(function () {
+        $('#lang').val('fr');
+        lang_toggle('fr');
+    });
+}
+
+function lang_toggle(language) {
+    $.ajax({
+        url: 'lang/translations.xml',
+        success: function (xml) {
+            $(xml).find('translation').each(function () {
+                var id = $(this).attr('id');
+                var text = $(this).find(language).text();
+                $("." + id).html(text);
+            });
+        }
+    });
 }
